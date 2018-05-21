17c819392db63b8f4d52c987529e8f63122ce7d2
ranarmoni <34243491+ranarmoni@users.noreply.github.com>
2018-05-21 23:41:19 +0300

    merge

--- game/main.c
+++ game/main.c
@@ -1,23 +1,22 @@
-
 #include "game.h"
 #include "solver.h"
 #include "parser.h"
 
 
 int main(){
+<<<<<<< HEAD
 	int command[4];
+=======
+	int true = 1;
 	int *command;
 >>>>>>> master
 	startGame();
 	while(!isGameOver){
 		command = readCommand();
-		if(command[0]==0 && !isGameOver()){ /* command is "set" */
 		if(command[0]==1 && !isGameOver()){ /* command is "set" */
 			setCell(command[1], command[2], command[3]);
 			continue;
 		}
-		if(command[0]==1 && !isGameOver()){ /* command is "hint" */
-			hintCell(command[1],command[2])
 		if(command[0]==2 && !isGameOver()){ /* command is "hint" */
 			hintCell(command[1],command[2]);
 			continue;
